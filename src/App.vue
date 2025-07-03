5ad2b07fa395ff07daeb026975e597c86c13db31
Edilson Junior <edilson99noslide@gmail.com>
2025-07-02 23:14:59 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  // COMPONENT
+  import Home from './components/Home.vue';
+</script>
+
+<template>
+  <div class="app__container">
+    <Home />
+  </div>
+</template>
+
+<style scoped>
+.app__container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 100vh;
+}
+</style>
